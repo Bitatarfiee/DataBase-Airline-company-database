461734673ff4cc35f16288e4c04b2c479905aeb1
Bita <b.tarfiee@gmail.com>
2024-10-21 20:26:27 +0200

    revised the code comments

--- help_functions.sql
+++ help_functions.sql
@@ -1,5 +1,5 @@
 /*
-Q4a: Help-function: Calculate the number of available seats for a certain flight
+Calculate the number of available seats for a certain flight
 */
 
 DROP function IF EXISTS `calculateFreeSeats`; 
@@ -38,32 +38,32 @@ BEGIN
     DECLARE weekDay VARCHAR(30);
 
 
-    -- Step 1: Get the rout number associated with the flight
+
     SELECT WS.R_Rout_Number INTO routNumber
     FROM Flight F
     JOIN WeeklySchedule WS ON F.WS_Schedule_ID = WS.ScheduleID
     WHERE F.Flight_Number = flightNumber;
     
-    -- Step 2: Get the root price using the rout number
+   
     SELECT RYD.Rout_Price INTO routPrice
     FROM Rout_Year_Dependency RYD
     WHERE RYD.R_Rout_Number = routNumber;
     
     
-      -- Step 1: Get the weekday associated with the flight
+  
     SELECT WS.D_Week_Day INTO weekDay
     FROM Flight F
     JOIN WeeklySchedule WS ON F.WS_Schedule_ID = WS.ScheduleID
     WHERE F.Flight_Number = flightNumber;
     
-    -- Step 2: Get the weekday factor using the week day
+
     SELECT WeekDay_Factor INTO weekdayFactor
     FROM Day D
     WHERE D.Week_Day = weekDay;
     
     
     
-    -- Step 3: Get the ScheduleID and Year_Code from the WeeklySchedule table using the flight number
+   
     SELECT WS.ScheduleID, WS.Year_Code INTO scheduleID, yearCode
     FROM Flight F
     JOIN WeeklySchedule WS ON F.WS_Schedule_ID = WS.ScheduleID
